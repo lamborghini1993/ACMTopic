f02ae28c5c0c3b0dc8a26cf6b4457fa663ce2574
duoyi-window <1323242382@qq.com>
2018-07-24 21:31:49 +0800

    模板template

---
+++ template.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <iterator>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+#define si1(a) scanf("%d",&a)
+#define si2(a,b) scanf("%d%d",&a,&b)
+#define sd1(a) scanf("%lf",&a)
+#define sd2(a,b) scanf("%lf%lf",&a,&b)
+#define ss1(s)  scanf("%s",s)
+#define pi1(a)    printf("%d\n",a)
+#define pi2(a,b)  printf("%d %d\n",a,b)
+#define pi64(a) printf("%I64d\n", a)
+#define pll(a) printf("%lld\n", a)
+#define mset(a,b)   memset(a,b,sizeof(a))
+#define forb(i,a,b)   for(int i=a;i<b;i++)
+#define ford(i,a,b)   for(int i=a;i<=b;i++)
+
+typedef __int64 I64;
+typedef long long LL;
+const int N = 40000;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+const double eps = 1e-7;
+
+int main()
+{
+    return 0;
+}
